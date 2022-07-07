8636f3210d7cdbe1760dfc17ba79c796b7bf94e5
nasotoamezcua <nasotoamezcua@gmail.com>
2022-07-07 12:51:06 -0500

    Se agrega microservico server config

--- microservicio-item-r4j/src/main/java/com/tecnonessystem/api/ItemController.java
+++ microservicio-item-r4j/src/main/java/com/tecnonessystem/api/ItemController.java
@@ -1,10 +1,19 @@
 package com.tecnonessystem.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,12 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tecnonessystem.model.Item;
 import com.tecnonessystem.service.IItemService;
 
+@RefreshScope
 @RestController
 public class ItemController {
+	
+	private Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	@Qualifier("itemServiceFeingCliente")
 	private IItemService service;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(
@@ -43,6 +61,23 @@ public class ItemController {
 	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
 	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return service.findById3(id, cantidad);		
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		
+		logger.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
--- microservicio-item-v1/src/main/java/com/tecnonessystem/api/ItemController.java
+++ microservicio-item-v1/src/main/java/com/tecnonessystem/api/ItemController.java
@@ -1,9 +1,18 @@
 package com.tecnonessystem.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,12 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tecnonessystem.model.Item;
 import com.tecnonessystem.service.IItemService;
 
+@RefreshScope
 @RestController
 public class ItemController {
+	
+	private Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	@Qualifier("itemServiceFeingCliente")
 	private IItemService service;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(
@@ -32,6 +50,23 @@ public class ItemController {
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return service.findById(id, cantidad);		
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		
+		logger.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
